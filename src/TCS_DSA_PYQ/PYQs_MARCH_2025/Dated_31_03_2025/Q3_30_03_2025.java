94023ea19d7bfd1d74c6771d14212d3b2c2ec31d
Ritesh Mane <riteshmane8308@gmail.com>
2026-03-31 10:59:12 +0530

    Q3_30_03_2025 ---> Solved with recursion approach.

--- src/TCS_DSA_PYQ/PYQs_MARCH_2025/Dated_31_03_2025/Q3_30_03_2025.java
+++ src/TCS_DSA_PYQ/PYQs_MARCH_2025/Dated_31_03_2025/Q3_30_03_2025.java
@@ -14,41 +14,80 @@
  * Explanation: The subset {3, 7} sums to 10.
  * */
 
+// DP_Apparoach:
+//package TCS_DSA_PYQ.PYQs_MARCH_2025.Dated_31_03_2025;
+//import java.util.*;
+//
+//public class Q3_30_03_2025 {
+//
+//
+//    public static  String doSubsetExists(int[] arr, int tSum) {
+//        int n = arr.length;
+//        boolean[][] dp = new boolean[n + 1][tSum + 1];
+//
+//        // Base case: sum 0 is always possible
+//        for (int i = 0; i <= n; i++) {
+//            dp[i][0] = true;
+//        }
+//
+//        // Fill DP table
+//        for (int i = 1; i <= n; i++) {
+//            for (int j = 1; j <= tSum; j++) {
+//                // Exclude current element
+//                dp[i][j] = dp[i - 1][j];
+//
+//                // Include current element if possible
+//                if (j >= arr[i - 1]) {
+//                    dp[i][j] = dp[i][j] || dp[i - 1][j - arr[i - 1]];
+//                }
+//            }
+//        }
+//
+//        return dp[n][tSum] ? "Yes":"No";
+//    }
+//
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//
+//        int n = sc.nextInt();
+//        int[] arr = new int[n];
+//        for(int i=0; i<n; i++) {
+//            arr[i] = sc.nextInt();
+//        }
+//
+//        int tSum = sc.nextInt();
+//
+//        System.out.println(doSubsetExists(arr, tSum));
+//
+//        sc.close();
+//    }
+//}
+//
 
+
+
+
+
+// Recursion Approach:
 package TCS_DSA_PYQ.PYQs_MARCH_2025.Dated_31_03_2025;
 import java.util.*;
 
 public class Q3_30_03_2025 {
 
+    public static boolean isSubsetSum(int[] arr, int n, int tSum) {
+        if(tSum == 0)   return true;
+        if(n == 0)  return false;
+        if(arr[n-1] > tSum) {
 
-    public static  String doSubsetExists(int[] arr, int tSum) {
-        int n = arr.length;
-        boolean[][] dp = new boolean[n + 1][tSum + 1];
-
-        // Base case: sum 0 is always possible
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = true;
+            return isSubsetSum(arr, n-1, tSum);
         }
 
-        // Fill DP table
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= tSum; j++) {
-                // Exclude current element
-                dp[i][j] = dp[i - 1][j];
-
-                // Include current element if possible
-                if (j >= arr[i - 1]) {
-                    dp[i][j] = dp[i][j] || dp[i - 1][j - arr[i - 1]];
-                }
-            }
-        }
-
-        return dp[n][tSum] ? "Yes":"No";
+        return (isSubsetSum(arr, n-1, tSum) || isSubsetSum(arr, n-1, tSum-arr[n-1]));
     }
 
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
         for(int i=0; i<n; i++) {
@@ -57,7 +96,11 @@ public class Q3_30_03_2025 {
 
         int tSum = sc.nextInt();
 
-        System.out.println(doSubsetExists(arr, tSum));
+        if(isSubsetSum(arr, n, tSum)) {
+            System.out.println("Yes.");
+        }else {
+            System.out.println("No");
+        }
 
         sc.close();
     }
